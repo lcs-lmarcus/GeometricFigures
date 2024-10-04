f04616b870c051b3c38cece72ca13b963361d3cb
Marcus Li <mmli@lcs.on.ca>
2024-10-04 12:05:02 -0400

    Added additional text views to display all the computed properties

--- GeometricFigures/Model/Circle.swift
+++ GeometricFigures/Model/Circle.swift
@@ -19,6 +19,7 @@ struct Circle: Describable {
     var radius: Double
     
     // MARK: Computed properties
+    // 2. Process
     var diameter: Double {
         return radius * 2
     }
--- GeometricFigures/Model/Views/CircleView.swift
+++ GeometricFigures/Model/Views/CircleView.swift
@@ -19,16 +19,25 @@ struct CircleView: View {
             
             // Label (describe what the slider is for)
             Text("Radius")
-            
+            // 1. INPUT
             // Slider control - to allow for user input
             Slider(
                 value: $currentCircle.radius,
                 in: 1...100,
                 step: 1.0
             )
-            
+            // Output
             // Label (show the current slider value)
-            Text("Diameter is \(currentCircle.radius.formatted())")
+            Text("Radius is \(currentCircle.radius.formatted())")
+            
+            // Label (show the diameter)
+            Text("Diameter is \(currentCircle.diameter.formatted())")
+            
+            // Label (show the area)
+            Text("Area is \(currentCircle.area.formatted())")
+            
+            // Label (show the circumference)
+            Text("Circumference is \(currentCircle.circumference.formatted())")
         }
     }
 }
